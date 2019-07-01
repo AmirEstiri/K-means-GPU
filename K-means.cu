1d356c57c93026f369e3dc016f052aded44b9c71
amirHosseinS3 <ahns3.ahe@gmail.com>
2019-07-02 02:40:54 +0430

    Debugging find center

--- K-means.cu
+++ K-means.cu
@@ -5,50 +5,59 @@
 #include <stdlib.h>
 #include <iostream>
 #include <ctime>
+#include <curand.h>
+#include <curand_kernel.h>
+#include <math.h>
+
 using namespace std;
 
 #define BLOCK_SIZE 1024
-#define n 100
-#define K 10
-#define d 3
+#define n 20
+#define K 3
+#define d 1
 
 struct Point{
-float val[d];
+    float val[d];
 };
+
+__global__ void setup_kernel(curandState *state){
+  int idx = threadIdx.x+blockDim.x*blockIdx.x;
+  curand_init(1234, idx, 0, &state[idx]);
+}
+
 
 __device__ float distance(Point* p1, Point* p2){
     float dist = 0;
     float tmp;
     for (int i = 0 ; i < d; i ++){
-            tmp = (p1->val[i] - p2->val[i]);
-            dist += tmp * tmp;
+        tmp = (p1->val[i] - p2->val[i]);
+        dist += tmp * tmp;
     }
     return dist;
 }
-__global__ void assignCluster(Point *data, int *cluster, Point *centers)
-{
+
+__global__ void assignCluster(Point *data, int *cluster, Point *centers){
     int tid = blockIdx.x * blockDim.x + threadIdx.x;
     if (tid > n)
-            return;
+         return;
 
     int best_cluster = 0;
     float best_distance = 1e10;
     float tmp;
 
     for (int i = 0 ; i < K; i++){
-            tmp = distance(data + i, centers + i);
-            if (tmp > best_distance) {
-                    best_distance = tmp;
-                    best_cluster = i;
-            }
+        tmp = distance(data + tid, centers + i);
+        if (tmp < best_distance) {
+            best_distance = tmp;
+            best_cluster = i;
+        }
     }
-cluster[tid] = best_cluster;
+    cluster[tid] = best_cluster;
 
 }
 
 
-__global__ void updateCenters(Point* data, int* cluster, Point* centers, int* cluster_sizes)
-{
+__global__ void updateCenters(Point* data, int* cluster, Point* centers, int* cluster_sizes, curandState *my_curandstate){
 
     int tid = blockIdx.x*blockDim.x + threadIdx.x;
     if (tid >= n) return;
@@ -67,8 +76,8 @@ __global__ void updateCenters(Point* data, int* cluster, Point* centers, int* cl
     data_shared[id]= data[tid];
 
     if (tid < K){
-      for (int i = 0 ; i < d; i++)
-          centers[tid].val[i] = 0;
+        for (int i = 0 ; i < d; i++)
+            centers[tid].val[i] = 0;
         cluster_sizes[tid] = 0;
     }
 __syncthreads();
@@ -88,7 +97,7 @@ for (int c = 0 ; c < K ; c++){
     for (int s = blockDim.x/2; s > 0; s /= 2) {
         if (id < s)
             for (int ind = 0; ind < d; ind++)
-        cluster_sum[id].val[ind] += cluster_sum[id+s].val[ind];
+                cluster_sum[id].val[ind] += cluster_sum[id+s].val[ind];
         __syncthreads();
     }
 
@@ -98,10 +107,19 @@ for (int c = 0 ; c < K ; c++){
         atomicAdd(cluster_sizes + c, cluster_sizes_shared[c]);
     }
 }
-__syncthreads();
+    __syncthreads();
     if(tid < K){
-        for (int ind = 0; ind < d; ind++)
-        centers[tid].val[ind] = centers[tid].val[ind]/cluster_sizes[tid];
+        if (cluster_sizes[tid] == 0)
+          for (int ind = 0; ind < d; ind++){
+            float myrandf = curand_uniform(my_curandstate+tid);
+            myrandf *= (1000);
+            myrandf -= 500;
+            int myrand = (int)truncf(myrandf);
+            centers[tid].val[ind] = myrand;
+          }
+        else
+          for (int ind = 0; ind < d; ind++)
+            centers[tid].val[ind] = centers[tid].val[ind] / cluster_sizes[tid];
     }
 
 }
@@ -112,10 +130,12 @@ int main(void)
     Point *data;
     int *cluster;
     Point *centers;
+    int *cluster_sizes;
 
     Point *d_data;
     int *d_cluster;
     Point *d_centers;
+    int *d_cluster_sizes;
 
     int size_data = n * sizeof(Point);
     int size_cluster = n * sizeof(int);
@@ -124,34 +144,79 @@ int main(void)
     data = (Point*)malloc(size_data);
     cluster = (int*)malloc(size_cluster);
     centers = (Point*)malloc(size_centers);
+    cluster_sizes = (int*)malloc(size_centers);
 
     cudaMalloc((void**)&d_data, size_data);
     cudaMalloc((void**)&d_cluster, size_cluster);
-cudaMalloc((void**)&d_centers, size_centers);
+    cudaMalloc((void**)&d_centers, size_centers);
+    cudaMalloc((void**)&d_cluster_sizes, size_centers);
 
-int i, j;
-for(i=0 ; i<n ; i++){
-    for (j=0; j<d; j++)
-        (data[i].val)[j] = i;
-}
+    curandState *d_state;
+    cudaMalloc(&d_state, sizeof(curandState));
+    setup_kernel<<<1,1>>>(d_state);
 
-for(i=0 ; i<K ; i++){
-    for (j=0; j<d; j++)
-        (centers[i].val)[j] = (rand() % 10);
-}
+    int i, j;
+    for(i=0 ; i<n ; i++){
+        for (j=0; j<d; j++)
+            (data[i].val)[j] = i;
+    }
 
-for (i=0; i<n; i++)
-    cluster[i] = 0;
+    // for(i=0 ; i<K ; i++){
+        // for (j=0; j<d; j++)
+            // (centers[i].val)[j] = (rand() % 10);
+    // }
 
-cudaMemcpy(d_data, data, size_data, cudaMemcpyHostToDevice);
-cudaMemcpy(d_centers, centers, size_centers, cudaMemcpyHostToDevice);
+    (centers[0].val)[0] = 10.1;
+    (centers[1].val)[0] = 18.2;
+    (centers[2].val)[0] = 4.5;
 
-dim3 dimBlock(n);
-dim3 dimGrid(1);
+    for (i=0; i<n; i++)
+        cluster[i] = 0;
 
-// printf("%f\n", distance(data, data + 1));
+    cudaMemcpy(d_data, data, size_data, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_cluster, cluster, size_cluster, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_centers, centers, size_centers, cudaMemcpyHostToDevice);
 
-assignCluster <<< dimGrid, dimBlock >>> (d_data, d_cluster, d_centers);
-cudaMemcpy( cluster, d_cluster, size_cluster, cudaMemcpyDeviceToHost );
+    int block_size;
+    int grid_size;
+    if (n < 1024){
+        block_size = n;
+        grid_size = 1;
+    } else{
+        block_size = 1;
+        grid_size = 1;
+        if (n % BLOCK_SIZE != 0) {
+            grid_size = n - (n % BLOCK_SIZE) + BLOCK_SIZE;
+            grid_size /= BLOCK_SIZE;
+        }
+    }
+
+    dim3 dimBlock(block_size);
+    dim3 dimGrid(grid_size);
+
+    // printf("%f\n", distance(data, data + 1));
+
+    for (int i=0; i<K; i++)
+        printf("%f, ", (centers[i].val)[0]);
+
+    printf("\n");
+
+    assignCluster <<< dimGrid, dimBlock >>> (d_data, d_cluster, d_centers);
+    updateCenters <<< dimGrid, dimBlock >>> (d_data, d_cluster, d_centers, d_cluster_sizes, d_state);
+
+    cudaMemcpy( centers, d_centers, size_centers, cudaMemcpyDeviceToHost );
+    cudaMemcpy( cluster, d_cluster, size_cluster, cudaMemcpyDeviceToHost );
+    cudaMemcpy( cluster_sizes, d_cluster_sizes, size_centers, cudaMemcpyDeviceToHost );
+
+    for (int i=0; i<K; i++)
+        printf("%f, ", (centers[i].val)[0]);
+
+    printf("\n");
+
+    for (int i=0; i<n; i++)
+        printf("%d, ", cluster[i]);
+
+    printf("\n");
+
     return 0;
 }
